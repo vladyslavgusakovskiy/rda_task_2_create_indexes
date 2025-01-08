d10911ed02d3a1505f43d6579035d15e558b374b
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-01-08 19:54:28 +0200

    Indexes have been created

--- task.sql
+++ task.sql
@@ -1,2 +1,7 @@
 # Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE INDEX idx_email ON Customers (Email);
+CREATE INDEX idx_name ON Products (Name);
+CREATE INDEX idx_customerid ON Orders (CustomerID);
+CREATE INDEX idx_orderid ON OrderItems (OrderID);
