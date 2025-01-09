c9c5a3a70fa1559e8885c14710d8cee8a3e07767
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-01-09 20:07:51 +0200

    added tables to the task.sql

--- task.sql
+++ task.sql
@@ -1,7 +1,23 @@
 CREATE DATABASE IF NOT EXISTS ShopDB;
 USE ShopDB;
 
-CREATE INDEX idx_email ON Customers (Email);
+CREATE TABLE IF NOT EXISTS Products (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Description VARCHAR(100),
+    Price INT,
+    WarehouseAmount INT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS Customers (
+    ID INT AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Address VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
 CREATE INDEX idx_name ON Products (Name);
-CREATE INDEX idx_customerid ON Orders (CustomerID);
-CREATE INDEX idx_orderid ON OrderItems (OrderID);
+CREATE INDEX idx_email ON Customers (Email);
